09908d5af8f63b20c93965e9be0fb5587f86314c
Marcio Roberto Andrade Souza Filho <marciorasf@gmail.com>
2020-06-06 04:34:39 -0300

    Mudanças no server para fazer o deploy no heroku como uma única aplicação.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 const Routes = require("./routes");
 
 const apiPort = 3000;
@@ -10,10 +11,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use(express.static("../client/build"));
+
 app.use("/api", Routes);
 
-app.all("*", function (req, res) {
-  res.send("Rota não mapeada.");
+app.use(function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 const server = app.listen(apiPort || 8080, function () {
