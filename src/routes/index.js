23a185e5d30718d22ada44a077c008e2ecdfa5da
marciorasf <marciorasf@gmail.com>
2021-05-23 11:48:59 -0300

    add healthcheck endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,5 +5,8 @@ const router = express.Router();
 router.post("/simularEmprestimo", Controller.simularEmprestimo);
 router.post("/efetivarEmprestimo", Controller.efetivarEmprestimo);
 router.get("/emprestimosSolicitados", Controller.getEmprestimosSolicitados);
+router.get("/", (req, res) => {
+  res.send("Hello from Picsize Test API!")
+})
 
 module.exports = router;
